73f9e1fe2451a567ef7468fc20ab2390c8d834c5
tracer-ysyx <tracer@ysyx.org>
2025-03-12 20:15:56 +0800

    > sim RTL ysyx_22040000 余意 Linux archlinux 6.13.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 27 Feb 2025 18:09:44 +0000 x86_64 GNU/Linux  20:15:56 up  5:30,  1 user,  load average: 0.72, 0.58, 0.45

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <nvboard.h>
 #include "verilated.h"
-#include "Vswitch.h"
+#include "Vlight.h"
 #include "verilated_fst_c.h"
 
+static TOP_NAME *top;
+
+void single_cycle();
+void reset(int n);
+void nvboard_bind_all_pins(TOP_NAME* top);
 
 int main(int argc, char **argv) {
+  nvboard_bind_all_pins(top);
+  nvboard_init();
+
   VerilatedContext *contextp = new VerilatedContext;
-  Vswitch* top= new Vswitch;
   VerilatedFstC* tfp = new VerilatedFstC;
 
   contextp->commandArgs(argc, argv);
@@ -16,19 +24,11 @@ int main(int argc, char **argv) {
   top->trace(tfp, 0);
   tfp->open("./build/wave.fst");
 
-  top->a = 0;
-  top->b = 0;
+  reset(10);
 
-  int i;
-  for (i = 0; i < 100; i++) {
-    int a = rand() & 1;
-    int b = rand() & 1;
-    top->a = a;
-    top->b = b;
-    top->eval();
-    printf("a = %d, b = %d, f = %d\n", top->a, top->b, top->f);
-    assert(top->f == (a ^ b));
-
+  while (1) {
+    nvboard_update();
+    single_cycle();
     tfp->dump(contextp->time());
     contextp->timeInc(1);
   }
@@ -40,3 +40,16 @@ int main(int argc, char **argv) {
 
   return 0;
 }
+
+void single_cycle() {
+  top->clk = 0; top->eval();
+  top->clk = 1; top->eval();
+}
+
+void reset(int n) {
+  top->rst = 1;
+  while (n -- > 0) single_cycle();
+  top->rst = 0;
+}
+
+
