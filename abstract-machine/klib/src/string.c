07bb05d00c89c7ea286e00428d7ef44321cfab0a
tracer-ysyx <tracer@ysyx.org>
2025-03-29 16:33:32 +0800

    > compile NEMU ysyx_25020081 余意 Linux archlinux 6.13.8-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 23 Mar 2025 17:17:30 +0000 x86_64 GNU/Linux  16:33:32 up  6:08,  1 user,  load average: 0.42, 0.32, 0.34

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -53,7 +53,7 @@ int strcmp(const char *s1, const char *s2) {
   assert(s1 != NULL && s2 != NULL);
   size_t i = 0;
 
-  while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]) {
+  while (s1[i] != '\0' && s1[i] == s2[i]) {
     i++;
   }
 
