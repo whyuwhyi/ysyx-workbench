35e7a4d8aef89e6c1fd526aeba167bdc1d1ad6b4
tracer-ysyx <tracer@ysyx.org>
2025-07-15 14:47:09 +0800

    > compile NEMU ysyx_25020081 余意 Linux archlinux 6.15.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 10 Jul 2025 17:10:18 +0000 x86_64 GNU/Linux  14:47:09 up 3 days, 23:20,  1 user,  load average: 0.73, 0.56, 0.42

--- nemu/src/trace/mtrace/mtrace.c
+++ nemu/src/trace/mtrace/mtrace.c
@@ -1,22 +1,21 @@
 #include <common.h>
 #include <memory/paddr.h>
 
+void init_mtrace() { Log("Memory trace initialized"); }
+
 void mtrace_read(paddr_t addr, int len) {
   if (addr >= PMEM_LEFT && addr <= PMEM_RIGHT) {
-    Log("Memory read from " FMT_PADDR " length %d\n", addr, len);
+    Log("Memory read from " FMT_PADDR " length %d", addr, len);
   } else {
-    Log("Invalid memory read from " FMT_PADDR " length %d\n", addr, len);
+    Log("Invalid memory read from " FMT_PADDR " length %d", addr, len);
   }
 }
 
 void mtrace_write(paddr_t addr, int len, word_t data) {
   if (addr >= PMEM_LEFT && addr <= PMEM_RIGHT) {
-    Log("Memory write to " FMT_PADDR " length %d data 0x%08x\n", addr, len,
-        data);
+    Log("Memory write to " FMT_PADDR " length %d data 0x%08x", addr, len, data);
   } else {
-    Log("Invalid memory write to " FMT_PADDR " length %d data 0x%08x\n", addr,
+    Log("Invalid memory write to " FMT_PADDR " length %d data 0x%08x", addr,
         len, data);
   }
 }
-
-void init_mtrace() { Log("Memory trace initialized\n"); }
