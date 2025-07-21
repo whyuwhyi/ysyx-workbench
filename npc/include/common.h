2e3df18840044cc82a903faa38eedff1ff458df3
tracer-ysyx <tracer@ysyx.org>
2025-07-22 00:50:42 +0800

    > compile NEMU ysyx_25030081 余意 Linux archlinux 6.15.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 17 Jul 2025 21:05:29 +0000 x86_64 GNU/Linux  00:50:42 up 11:51,  2 users,  load average: 1.57, 1.53, 1.46

--- npc/include/common.h
+++ npc/include/common.h
@@ -1,27 +1,27 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NPC is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+ *
+ * NPC is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#include <stdint.h>
+#include <assert.h>
 #include <inttypes.h>
 #include <stdbool.h>
-#include <string.h>
-#include <assert.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <generated/autoconf.h>
 #include <macro.h>
@@ -31,7 +31,7 @@
 #endif
 
 typedef uint32_t word_t;
-typedef int32_t  sword_t;
+typedef int32_t sword_t;
 #define FMT_WORD "0x%08" PRIx32
 
 typedef word_t vaddr_t;
