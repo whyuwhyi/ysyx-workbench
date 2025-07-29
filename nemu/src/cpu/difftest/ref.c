6226b8c5f9cd59847764c3334ef2a607e188d182
tracer-ysyx <tracer@ysyx.org>
2025-07-29 18:55:37 +0800

    > compile NEMU ysyx_25030081 余意 Linux archlinux 6.15.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 17 Jul 2025 21:05:29 +0000 x86_64 GNU/Linux  18:55:37 up  3:50,  1 user,  load average: 0.41, 0.29, 0.44

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -1,50 +1,43 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
-#include <isa.h>
 #include <cpu/cpu.h>
 #include <difftest-def.h>
+#include <isa.h>
 #include <memory/paddr.h>
 
-__EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
+__EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n,
+                              bool direction) {
   if (direction == DIFFTEST_TO_REF) {
-    // Copy from buf to NEMU memory
     memcpy(guest_to_host(addr), buf, n);
   } else {
-    // Copy from NEMU memory to buf
     memcpy(buf, guest_to_host(addr), n);
   }
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
   if (direction == DIFFTEST_TO_REF) {
-    // Copy DUT registers to NEMU
     memcpy(&cpu, dut, sizeof(cpu));
   } else {
-    // Copy NEMU registers to DUT
     memcpy(dut, &cpu, sizeof(cpu));
   }
 }
 
-__EXPORT void difftest_exec(uint64_t n) {
-  cpu_exec(n);
-}
+__EXPORT void difftest_exec(uint64_t n) { cpu_exec(n); }
 
-__EXPORT void difftest_raise_intr(word_t NO) {
-  assert(0);
-}
+__EXPORT void difftest_raise_intr(word_t NO) { assert(0); }
 
 __EXPORT void difftest_init(int port) {
   void init_mem();
