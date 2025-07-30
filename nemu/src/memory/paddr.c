93e4c43ad0da1d7931c6fcb0a0720d3b1e2bffbe
tracer-ysyx <tracer@ysyx.org>
2025-07-30 22:51:26 +0800

    > compile NEMU ysyx_25030081 余意 Linux bogon 6.15.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 17 Jul 2025 21:05:29 +0000 x86_64 GNU/Linux  22:51:26 up 1 day,  7:46,  1 user,  load average: 0.56, 0.55, 0.54

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -61,6 +61,7 @@ word_t paddr_read(paddr_t addr, int len) {
     IFDEF(CONFIG_MTRACE, mtrace_read(addr, len));
     return pmem_read(addr, len);
   }
+  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
@@ -71,5 +72,6 @@ void paddr_write(paddr_t addr, int len, word_t data) {
     pmem_write(addr, len, data);
     return;
   }
+  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
