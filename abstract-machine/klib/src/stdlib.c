484cf3d07f1451df83ccbb1ea531b58e2dafc14e
tracer-ysyx <tracer@ysyx.org>
2025-07-30 18:28:51 +0800

    > compile NEMU ysyx_25030081 余意 Linux bogon 6.15.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 17 Jul 2025 21:05:29 +0000 x86_64 GNU/Linux  18:28:51 up 1 day,  3:23,  1 user,  load average: 0.61, 0.54, 0.38

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -30,12 +30,6 @@ int atoi(const char *nptr) {
 
 static uintptr_t addr = 0;
 void *malloc(size_t size) {
-  // On native, malloc() will be called during initializaion of C runtime.
-  // Therefore do not call panic() here, else it will yield a dead recursion:
-  //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
-#endif
   if (addr == 0) {
     addr = (uintptr_t)heap.start;
   }
