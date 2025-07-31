c93f42908f26fa95642861ec3088b6b2a0bdfb90
tracer-ysyx <tracer@ysyx.org>
2025-07-31 16:38:31 +0800

    > compile NEMU ysyx_25030081 余意 Linux bogon 6.15.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 17 Jul 2025 21:05:29 +0000 x86_64 GNU/Linux  16:38:31 up 2 days,  1:33,  1 user,  load average: 0.58, 0.62, 0.66

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,15 +5,15 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
 
-  int KEY_CODE = (int)inl(KBD_ADDR);
-  if (KEY_CODE == 0) {
+  int key_code = (int)inl(KBD_ADDR);
+  if (key_code == 0) {
     kbd->keydown = false;
     kbd->keycode = AM_KEY_NONE;
-  } else if (KEY_CODE & KEYDOWN_MASK) {
+  } else if (key_code & KEYDOWN_MASK) {
     kbd->keydown = true;
-    kbd->keycode = KEY_CODE & 0x7fff;
+    kbd->keycode = key_code & 0x7fff;
   } else {
     kbd->keydown = false;
-    kbd->keycode = KEY_CODE;
+    kbd->keycode = key_code;
   }
 }
