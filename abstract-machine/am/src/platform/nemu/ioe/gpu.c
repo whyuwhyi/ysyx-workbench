fb4f46d789c9dd3d2799e5f1ce5b19b2f180c9f1
tracer-ysyx <tracer@ysyx.org>
2025-07-31 22:50:25 +0800

    > compile NEMU ysyx_25030081 余意 Linux archlinux 6.15.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 17 Jul 2025 21:05:29 +0000 x86_64 GNU/Linux  22:50:25 up 2 days,  7:45,  1 user,  load average: 0.69, 0.50, 0.30

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -7,20 +7,20 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  int i;
-  int w = inw(WIDTH_ADDR);
-  int h = inw(HEIGHT_ADDR);
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i++)
-    fb[i] = i;
-  outl(SYNC_ADDR, 1);
+  // int i;
+  // int w = (int)inw(WIDTH_ADDR);
+  // int h = (int)inw(HEIGHT_ADDR);
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < w * h; i++)
+  //   fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T){.present = true,
                            .has_accel = false,
-                           .width = inw(WIDTH_ADDR),
-                           .height = inw(HEIGHT_ADDR),
+                           .width = (int)inw(WIDTH_ADDR),
+                           .height = (int)inw(HEIGHT_ADDR),
                            .vmemsz = 0};
 }
 
@@ -29,8 +29,8 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int y = ctl->y;
   int w = ctl->w;
   int h = ctl->h;
-  int width = inw(WIDTH_ADDR);
-  int height = inw(HEIGHT_ADDR);
+  int width = (int)inw(WIDTH_ADDR);
+  int height = (int)inw(HEIGHT_ADDR);
   uint32_t *pixels = (uint32_t *)ctl->pixels;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
 
