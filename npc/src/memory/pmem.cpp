b6f8143665bfe1685cfd500165b8077c6a127848
tracer-ysyx <tracer@ysyx.org>
2025-07-31 23:40:43 +0800

    > sim RTL ysyx_25030081 余意 Linux bogon 6.15.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 17 Jul 2025 21:05:29 +0000 x86_64 GNU/Linux  23:40:43 up 2 days,  8:35,  1 user,  load average: 1.10, 0.93, 0.73

--- npc/src/memory/pmem.cpp
+++ npc/src/memory/pmem.cpp
@@ -112,9 +112,7 @@ extern "C" int pmem_read(int raddr) {
 }
 
 extern "C" void pmem_write(int waddr, int wdata, char wmask) {
-  if (waddr == (uint32_t)SERIAL_ADDR) {
-    Log("Serial write: 0x%08x, data: 0x%08x, mask: 0x%02x", waddr, wdata,
-        wmask);
+  if (waddr == SERIAL_ADDR) {
     serial_putchar(wdata & 0xff);
     return;
   }
