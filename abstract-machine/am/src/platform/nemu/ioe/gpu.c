08a0c3ee4ca8acc403bf39ddae6c03489e6de158
tracer-ysyx <tracer@ysyx.org>
2025-07-31 21:42:52 +0800

    > compile NEMU ysyx_25030081 余意 Linux archlinux 6.15.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 17 Jul 2025 21:05:29 +0000 x86_64 GNU/Linux  21:42:52 up 2 days,  6:37,  1 user,  load average: 0.49, 0.44, 0.37

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,17 +1,26 @@
 #include <am.h>
 #include <nemu.h>
 
+#define WIDTH_ADDR VGACTL_ADDR
+#define HEIGHT_ADDR (VGACTL_ADDR + 2)
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  int w = inw(WIDTH_ADDR) / 32;
+  int h = inw(HEIGHT_ADDR) / 32;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i++)
+    fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+  *cfg = (AM_GPU_CONFIG_T){.present = true,
+                           .has_accel = false,
+                           .width = inw(WIDTH_ADDR),
+                           .height = inw(HEIGHT_ADDR),
+                           .vmemsz = 0};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
@@ -20,6 +29,4 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   }
 }
 
-void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
-}
+void __am_gpu_status(AM_GPU_STATUS_T *status) { status->ready = true; }
--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -2,11 +2,14 @@
 #include <nemu.h>
 #include <stdint.h>
 
+#define RTC_ADDR_HIGH (RTC_ADDR + 4)
+#define RTC_ADDR_LOW RTC_ADDR
+
 void __am_timer_init() {}
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uint64_t high_us = inl(RTC_ADDR + 4);
-  uint64_t low_us = inl(RTC_ADDR);
+  uint64_t high_us = inl(RTC_ADDR_HIGH);
+  uint64_t low_us = inl(RTC_ADDR_LOW);
   uptime->us = (high_us << 32) | low_us;
 }
 
