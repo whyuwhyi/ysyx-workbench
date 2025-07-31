96ab0f8b364f73490b5aacef93b17a46f3aca1b4
tracer-ysyx <tracer@ysyx.org>
2025-07-31 22:54:17 +0800

    > compile NEMU ysyx_25030081 余意 Linux bogon 6.15.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 17 Jul 2025 21:05:29 +0000 x86_64 GNU/Linux  22:54:17 up 2 days,  7:48,  1 user,  load average: 1.10, 0.86, 0.50

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,18 +2,18 @@
 #include <nemu.h>
 #include <stdint.h>
 
-#define WIDTH_ADDR VGACTL_ADDR
-#define HEIGHT_ADDR (VGACTL_ADDR + 2)
+#define WIDTH_ADDR (VGACTL_ADDR + 2)
+#define HEIGHT_ADDR VGACTL_ADDR
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  // int i;
-  // int w = (int)inw(WIDTH_ADDR);
-  // int h = (int)inw(HEIGHT_ADDR);
-  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // for (i = 0; i < w * h; i++)
-  //   fb[i] = i;
-  // outl(SYNC_ADDR, 1);
+  int i;
+  int w = (int)inw(WIDTH_ADDR);
+  int h = (int)inw(HEIGHT_ADDR);
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i++)
+    fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
