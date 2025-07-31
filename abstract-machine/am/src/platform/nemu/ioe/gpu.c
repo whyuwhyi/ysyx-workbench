fd6be51f26aa0511376d4b02f30fafd35e3344d8
tracer-ysyx <tracer@ysyx.org>
2025-07-31 21:59:39 +0800

    > compile NEMU ysyx_25030081 余意 Linux archlinux 6.15.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 17 Jul 2025 21:05:29 +0000 x86_64 GNU/Linux  21:59:39 up 2 days,  6:54,  1 user,  load average: 1.01, 0.62, 0.50

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -24,6 +24,26 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int x = ctl->x;
+  int y = ctl->y;
+  int w = ctl->w;
+  int h = ctl->h;
+  int width = inw(WIDTH_ADDR);
+  int height = inw(HEIGHT_ADDR);
+  uint32_t *pixels = ctl->pixels;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+
+  for (int i = x; i < x + w && i < width; i++) {
+    for (int j = y; j < y + h && j < height; j++) {
+      int idx = j * width + i;
+      if (pixels) {
+        fb[idx] = pixels[(j - y) * w + (i - x)];
+      } else {
+        fb[idx] = 0;
+      }
+    }
+  }
+
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
