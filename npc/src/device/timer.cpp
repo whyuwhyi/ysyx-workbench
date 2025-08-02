aeacae000418a79b793ac0ed6935c6cdfd465cd1
tracer-ysyx <tracer@ysyx.org>
2025-08-02 23:32:15 +0800

    > sim RTL ysyx_25030081 余意 Linux archlinux 6.15.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 17 Jul 2025 21:05:29 +0000 x86_64 GNU/Linux  23:32:15 up 4 days,  8:26,  1 user,  load average: 0.53, 0.60, 0.49

--- npc/src/device/timer.cpp
+++ npc/src/device/timer.cpp
@@ -7,13 +7,13 @@ static uint64_t boot_time = 0;
 void timer_init() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
-  boot_time = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+  boot_time = tv.tv_sec * 1000000 + tv.tv_usec;
   Log("Timer initialized");
 }
 
 int timer_get_timer() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
-  uint64_t current_time = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+  uint64_t current_time = tv.tv_sec * 1000000 + tv.tv_usec;
   return (int)(current_time - boot_time);
 }
