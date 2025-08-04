8c2cb0f24923a0e39343ba4bc142dca1062b4da8
tracer-ysyx <tracer@ysyx.org>
2025-08-05 00:20:39 +0800

    > compile NEMU ysyx_25030081 余意 Linux bogon 6.15.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 17 Jul 2025 21:05:29 +0000 x86_64 GNU/Linux  00:20:39 up 6 days,  9:15,  1 user,  load average: 0.97, 0.77, 0.69

--- abstract-machine/klib/include/klib.h
+++ abstract-machine/klib/include/klib.h
@@ -16,30 +16,12 @@ void *memset(void *s, int c, size_t n);
 void *memcpy(void *dst, const void *src, size_t n);
 void *memmove(void *dst, const void *src, size_t n);
 int memcmp(const void *s1, const void *s2, size_t n);
-void *memchr(const void *s, int c, size_t n);
 size_t strlen(const char *s);
-size_t strnlen(const char *s, size_t maxlen);
 char *strcat(char *dst, const char *src);
-char *strncat(char *dst, const char *src, size_t n);
 char *strcpy(char *dst, const char *src);
 char *strncpy(char *dst, const char *src, size_t n);
 int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, size_t n);
-char *strchr(const char *s, int c);
-char *strrchr(const char *s, int c);
-char *strstr(const char *haystack, const char *needle);
-char *strdup(const char *s);
-char *strtok(char *str, const char *delim);
-int strcasecmp(const char *s1, const char *s2);
-int strncasecmp(const char *s1, const char *s2, size_t n);
-#ifndef _CTYPE_H
-#ifndef __CTYPE_H
-#ifndef _CTYPE_H_
-int tolower(int c);
-int toupper(int c);
-#endif
-#endif
-#endif
 
 // stdlib.h
 void srand(unsigned int seed);
