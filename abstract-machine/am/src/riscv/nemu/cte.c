e8ed846aa921e5c029d89282dc13c1ff1f424418
tracer-ysyx <tracer@ysyx.org>
2025-08-12 16:15:22 +0800

    > compile NEMU ysyx_25030081 余意 Linux bogon 6.15.9-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 02 Aug 2025 01:20:06 +0000 x86_64 GNU/Linux  16:15:22 up 1 day, 17:46,  1 user,  load average: 0.51, 0.57, 0.52

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -13,7 +13,15 @@ Context *__am_irq_handle(Context *c) {
       ev.event = EVENT_YIELD;
       break;
     case 0:
+    case 1:
+    case 2:
+    case 3:
     case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
       ev.event = EVENT_SYSCALL;
       break;
     default:
