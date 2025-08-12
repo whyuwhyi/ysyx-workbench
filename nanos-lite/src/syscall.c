dc62e3886c1c251e889962ca2e4c39c8c43872fc
tracer-ysyx <tracer@ysyx.org>
2025-08-12 16:07:26 +0800

    > compile NEMU ysyx_25030081 余意 Linux bogon 6.15.9-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 02 Aug 2025 01:20:06 +0000 x86_64 GNU/Linux  16:07:26 up 1 day, 17:38,  1 user,  load average: 0.81, 0.55, 0.48

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,7 @@
 #include "syscall.h"
 #include <common.h>
 
+static void sys_exit(int code) __attribute__((noreturn));
 static int sys_yield(void);
 
 void do_syscall(Context *c) {
@@ -8,6 +9,9 @@ void do_syscall(Context *c) {
   a[0] = c->GPR1;
 
   switch (a[0]) {
+  case SYS_exit:
+    sys_exit(c->GPR2);
+    break;
   case SYS_yield:
     c->GPRx = sys_yield();
     break;
@@ -15,6 +19,8 @@ void do_syscall(Context *c) {
     panic("Unhandled syscall ID = %d", a[0]);
   }
 }
+
+static void sys_exit(int code) { halt(code); }
 
 static int sys_yield(void) {
   yield();
