c8d0645b8ea4f6adae6bcbe8c4e2448e10f7e76c
tracer-ysyx <tracer@ysyx.org>
2025-08-22 01:08:02 +0800

    > sim RTL ysyx_25030081 余意 Linux Administrator 6.16.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 15 Aug 2025 16:04:43 +0000 x86_64 GNU/Linux  01:08:02 up 38 min,  1 user,  load average: 1.50, 1.03, 1.10

--- npc/sim/src/difftest/filelist.mk
+++ npc/sim/src/difftest/filelist.mk
@@ -1,6 +1,2 @@
 SRCS-$(CONFIG_DIFFTEST) += src/difftest/difftest.cpp
 
-ifdef CONFIG_DIFFTEST
-DIFF_REF_SO = $(NPC_HOME)/sim/$(call remove_quote,$(CONFIG_DIFFTEST_REF_PATH))
-ARGS_DIFF = --diff=$(DIFF_REF_SO)
-endif
