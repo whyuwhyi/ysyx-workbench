6d2806d8f95e612b701107c51a844a79d4a56abe
tracer-ysyx <tracer@ysyx.org>
2025-08-22 00:36:29 +0800

    > sim RTL ysyx_25030081 余意 Linux Administrator 6.16.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 15 Aug 2025 16:04:43 +0000 x86_64 GNU/Linux  00:36:29 up 6 min,  1 user,  load average: 1.11, 1.73, 0.94

--- npc/sim/include/generated/autoconf.h
+++ npc/sim/include/generated/autoconf.h
@@ -6,6 +6,8 @@
  */
 #define CONFIG_MSIZE 0x8000000
 #define CONFIG_ITRACE 1
+#define CONFIG_DIFFTEST 1
 #define CONFIG_WAVE_TRACE 1
 #define CONFIG_MBASE 0x80000000
+#define CONFIG_DIFFTEST_REF_PATH "tools/difftest/ref.so"
 #define CONFIG_TRACE 1
