3da4b0245b2ff7c8044bd33473385c90ce9bab4e
tracer-ysyx <tracer@ysyx.org>
2025-08-23 01:20:24 +0800

    > compile NEMU ysyx_25030081 余意 Linux archlinux 6.16.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 15 Aug 2025 16:04:43 +0000 x86_64 GNU/Linux  01:20:24 up 1 day, 50 min,  1 user,  load average: 0.38, 0.64, 0.54

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -57,7 +57,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 
 void yield() {
 #ifdef __riscv_e
-  asm volatile("li a5, -1; ecall");
+  asm volatile("li a5, 11; ecall");
 #else
   asm volatile("li a7, -1; ecall");
 #endif
