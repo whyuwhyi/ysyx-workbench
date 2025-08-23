a479b3a4c746c23a97bfbe3fec29ae1904d50084
tracer-ysyx <tracer@ysyx.org>
2025-08-24 03:33:42 +0800

    > sim RTL ysyx_25030081 余意 Linux archlinux 6.16.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 15 Aug 2025 16:04:43 +0000 x86_64 GNU/Linux  03:33:42 up 2 days,  3:03,  1 user,  load average: 0.77, 0.74, 0.74

--- navy-apps/scripts/riscv32.mk
+++ navy-apps/scripts/riscv32.mk
@@ -1,5 +1,4 @@
 include $(NAVY_HOME)/scripts/riscv/common.mk
 CROSS_COMPILE = riscv64-linux-gnu-
 CFLAGS  += -march=rv32g -mabi=ilp32  #overwrite
-CFLAGS  += -Wno-implicit-function-declaration
 LDFLAGS += -melf32lriscv
