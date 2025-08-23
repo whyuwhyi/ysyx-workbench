cafcbf5e251df4f97fb2c98ed8ac3ed6795de453
tracer-ysyx <tracer@ysyx.org>
2025-08-24 03:14:40 +0800

    > compile NEMU ysyx_25030081 余意 Linux archlinux 6.16.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 15 Aug 2025 16:04:43 +0000 x86_64 GNU/Linux  03:14:40 up 2 days,  2:44,  1 user,  load average: 0.63, 0.75, 0.74

--- nemu/src/trace/dtrace.c
+++ nemu/src/trace/dtrace.c
@@ -3,11 +3,11 @@
 void init_dtrace() { Log("Device trace initialized"); }
 
 void dtrace_read(paddr_t addr, int len, word_t data, const char *device) {
-  Log("Device read from " FMT_PADDR " length %d data " FMT_WORD "<@device %s>",
-      addr, len, data, device);
+  Log("Device read from " FMT_PADDR " length %d data " FMT_WORD "<@d%s>", addr,
+      len, data, device);
 }
 
 void dtrace_write(paddr_t addr, int len, word_t data, const char *device) {
-  Log("Device write to " FMT_PADDR " length %d data " FMT_WORD "<@device %s>",
-      addr, len, data, device);
+  Log("Device write to " FMT_PADDR " length %d data " FMT_WORD "<@%s>", addr,
+      len, data, device);
 }
