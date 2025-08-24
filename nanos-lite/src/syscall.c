2461772519a5557d0b839fe19049a72cd04b8d79
tracer-ysyx <tracer@ysyx.org>
2025-08-24 15:58:27 +0800

    > compile NEMU ysyx_25030081 余意 Linux archlinux 6.16.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 15 Aug 2025 16:04:43 +0000 x86_64 GNU/Linux  15:58:27 up 2 days, 15:28,  1 user,  load average: 0.52, 0.54, 0.53

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,8 +1,12 @@
 #include "syscall.h"
 #include <common.h>
 
+static void strace_call(int id);
 static void sys_exit(int code) __attribute__((noreturn));
 static int sys_yield(void);
+// static int sys_open(const char *path, int flags, int mode);
+// static int sys_read(int fd, void *buf, size_t count);
+static int sys_write(int fd, void *buf, size_t count);
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
@@ -15,9 +19,13 @@ void do_syscall(Context *c) {
   case SYS_yield:
     c->GPRx = sys_yield();
     break;
+  case SYS_write:
+    c->GPRx = sys_write(c->GPR2, (void *)c->GPR3, c->GPR4);
+    break;
   default:
     panic("Unhandled syscall ID = %d", a[0]);
   }
+  strace_call(a[0]);
 }
 
 static void sys_exit(int code) { halt(code); }
@@ -25,4 +33,24 @@ static void sys_exit(int code) { halt(code); }
 static int sys_yield(void) {
   yield();
   return 0;
+}
+
+static int sys_write(int fd, void *buf, size_t count) {
+  if (fd == 1 || fd == 2) {
+    for (size_t i = 0; i < count; i++) {
+      putch(((char *)buf)[i]);
+    }
+    return count;
+  }
+  return -1;
+}
+
+const char *syscall_name[] = {
+    "SYS_exit",  "SYS_yield",  "SYS_open",   "SYS_read",   "SYS_write",
+    "SYS_kill",  "SYS_getpid", "SYS_close",  "SYS_lseek",  "SYS_brk",
+    "SYS_fstat", "SYS_time",   "SYS_signal", "SYS_execve", "SYS_fork",
+    "SYS_link",  "SYS_unlink", "SYS_wait",   "SYS_times",  "SYS_gettimeofday"};
+
+static void strace_call(int id) {
+  Log("Syscall %s is invoked.", syscall_name[id]);
 }
