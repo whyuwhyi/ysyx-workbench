ec3e92316d865f0d4fc103f9ec5236a4b3d022b8
tracer-ysyx <tracer@ysyx.org>
2025-08-28 00:00:40 +0800

    > compile NEMU ysyx_25030081 余意 Linux bogon 6.16.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 23 Aug 2025 15:32:49 +0000 x86_64 GNU/Linux  00:00:40 up  9:27,  1 user,  load average: 1.01, 1.36, 1.59

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -3,27 +3,31 @@
 
 #define keyname(k) #k,
 
-static const char *keyname[] = {
-  "NONE",
-  _KEYS(keyname)
-};
+static const char *keyname[] = {"NONE", _KEYS(keyname)};
 
-int SDL_PushEvent(SDL_Event *ev) {
-  return 0;
-}
+int SDL_PushEvent(SDL_Event *ev) { return 0; }
 
-int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
-}
+int SDL_PollEvent(SDL_Event *ev) { return 0; }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
+  char buf[64];
+  char type[8], key_name[8];
+  int keycode;
+
+  while (1) {
+    if (NDL_PollEvent(buf, sizeof(buf)) == 0) {
+      continue;
+    }
+    sscanf(buf, "%s %s %d\n", type, key_name, &keycode);
+    event->type = buf[1] == 'u' ? SDL_KEYUP : SDL_KEYDOWN;
+    event->key.keysym.sym = keycode;
+    return 1;
+  }
+  return 0;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
   return 0;
 }
 
-uint8_t* SDL_GetKeyState(int *numkeys) {
-  return NULL;
-}
+uint8_t *SDL_GetKeyState(int *numkeys) { return NULL; }
