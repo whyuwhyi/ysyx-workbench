af2a649d3c66c1f0f0407495b1984494ef9184c9
tracer-ysyx <tracer@ysyx.org>
2025-08-28 00:23:40 +0800

    > compile NEMU ysyx_25030081 余意 Linux bogon 6.16.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 23 Aug 2025 15:32:49 +0000 x86_64 GNU/Linux  00:23:40 up  9:50,  1 user,  load average: 0.70, 0.64, 0.85

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -35,7 +35,7 @@ static Finfo file_table[] __attribute__((used)) = {
     [FD_STDIN] = {"stdin", 0, 0, invalid_read, invalid_write},
     [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
     [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
-    [FD_EVENT] = {"/dev/event", 0, 0, events_read, invalid_write},
+    [FD_EVENT] = {"/dev/events", 0, 0, events_read, invalid_write},
     [FD_DISPINFO] = {"/proc/dispinfo", 128, 0, dispinfo_read, invalid_write},
     [FD_FB] = {"/dev/fb", 0, 0, invalid_read, fb_write},
 #include "files.h"
