850af04c6d852950bc68073c0e8b2763264bebee
tracer-ysyx <tracer@ysyx.org>
2025-08-29 00:37:21 +0800

    > sim RTL ysyx_25030081 余意 Linux bogon 6.16.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 23 Aug 2025 15:32:49 +0000 x86_64 GNU/Linux  00:37:21 up 1 day, 10:04,  1 user,  load average: 1.05, 1.25, 1.15

--- navy-apps/libs/libam/src/trm.cpp
+++ navy-apps/libs/libam/src/trm.cpp
@@ -1,7 +1,7 @@
 #include <am.h>
 #include <stdlib.h>
 
-#define HEAP_SIZE (80 * 1024 * 1024)
+#define HEAP_SIZE (8 * 1024 * 1024)
 static unsigned char _heap[HEAP_SIZE];
 Area heap = {_heap, _heap + HEAP_SIZE};
 
