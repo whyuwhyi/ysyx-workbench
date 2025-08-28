8167b4125c9193102b2d4bc6f0ef4450f7245659
tracer-ysyx <tracer@ysyx.org>
2025-08-28 14:32:46 +0800

    > compile NEMU ysyx_25030081 余意 Linux bogon 6.16.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 23 Aug 2025 15:32:49 +0000 x86_64 GNU/Linux  14:32:46 up 23:59,  1 user,  load average: 0.62, 0.50, 0.49

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -65,6 +65,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
     write(fd, pixels + i * w, w * 4);
   }
   close(fd);
+  printf("NDL_DrawRect: (%d,%d) %dx%d\n", x, y, w, h);
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {}
