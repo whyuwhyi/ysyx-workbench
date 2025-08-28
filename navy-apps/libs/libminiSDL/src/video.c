aa389f6badf94e7869b6d62aadc2d0f766c27e09
tracer-ysyx <tracer@ysyx.org>
2025-08-28 14:03:19 +0800

    > compile NEMU ysyx_25030081 余意 Linux bogon 6.16.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 23 Aug 2025 15:32:49 +0000 x86_64 GNU/Linux  14:03:19 up 23:30,  1 user,  load average: 0.66, 0.49, 0.55

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -56,6 +56,11 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
+  assert(s);
+  if (w == 0 && h == 0) {
+    NDL_DrawRect((uint32_t *)s->pixels, 0, 0, s->w, s->h);
+    return;
+  }
   uint32_t *ptr = (uint32_t *)s->pixels;
   uint32_t buf[w * h];
   for (int i = 0; i < h; i++) {
