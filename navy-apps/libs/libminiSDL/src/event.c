48407ef4d06c25014160acbe065c0b3cced04806
tracer-ysyx <tracer@ysyx.org>
2025-08-28 16:48:13 +0800

    > compile NEMU ysyx_25030081 余意 Linux bogon 6.16.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 23 Aug 2025 15:32:49 +0000 x86_64 GNU/Linux  16:48:13 up 1 day,  2:15,  1 user,  load average: 0.68, 0.87, 0.94

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -7,7 +7,23 @@ static const char *keyname[] = {"NONE", _KEYS(keyname)};
 
 int SDL_PushEvent(SDL_Event *ev) { return 0; }
 
-int SDL_PollEvent(SDL_Event *ev) { return 0; }
+int SDL_PollEvent(SDL_Event *ev) {
+  char buf[64];
+  char type[8], key_name[8];
+  if (NDL_PollEvent(buf, sizeof(buf)) == 0) {
+    return 0;
+  }
+  sscanf(buf, "%s %s\n", type, key_name);
+  ev->type = (buf[1] == 'u') ? SDL_KEYUP : SDL_KEYDOWN;
+  for (int i = 0; i < sizeof(keyname) / sizeof(keyname[0]); i++) {
+    if (strcmp(key_name, keyname[i]) == 0) {
+      ev->key.keysym.sym = i;
+      return 1;
+    }
+  }
+
+  return 0;
+}
 
 int SDL_WaitEvent(SDL_Event *event) {
   char buf[64];
