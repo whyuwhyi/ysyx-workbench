df2fbdc0ff6832d2fb41bff6a86244ec46d86304
tracer-ysyx <tracer@ysyx.org>
2025-08-28 21:20:45 +0800

    > compile NEMU ysyx_25030081 余意 Linux archlinux 6.16.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 23 Aug 2025 15:32:49 +0000 x86_64 GNU/Linux  21:20:45 up 1 day,  6:47,  1 user,  load average: 0.81, 0.91, 0.67

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -58,7 +58,7 @@ void NDL_OpenCanvas(int *w, int *h) {
 }
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
-  // x += (screen_w - w) / 2;
+  x += (screen_w - w) / 2;
   // y += (screen_h - h) / 2;
   int fd = open("/dev/fb", 0);
   for (int i = 0; i < h; i++) {
