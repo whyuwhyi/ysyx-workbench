d791211af884a38f0372e11818d535fa25c7966a
tracer-ysyx <tracer@ysyx.org>
2025-08-30 00:56:08 +0800

    > compile NEMU ysyx_25030081 余意 Linux bogon 6.16.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 23 Aug 2025 15:32:49 +0000 x86_64 GNU/Linux  00:56:08 up 2 days, 10:23,  1 user,  load average: 0.55, 0.57, 0.62

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,7 @@ void do_syscall(Context *c);
 static Context *do_event(Event e, Context *c) {
   switch (e.event) {
   case EVENT_YIELD:
-    schedule(c);
+    c = schedule(c);
     break;
   case EVENT_SYSCALL:
     do_syscall(c);
