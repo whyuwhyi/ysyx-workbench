0ee302260f10dc251b04b92c62eb6c7793e5d06d
tracer-ysyx <tracer@ysyx.org>
2025-08-29 17:57:50 +0800

    > sim RTL ysyx_25030081 余意 Linux bogon 6.16.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 23 Aug 2025 15:32:49 +0000 x86_64 GNU/Linux  17:57:50 up 2 days,  3:24,  1 user,  load average: 0.52, 0.58, 0.64

--- navy-apps/libs/libam/src/trm.cpp
+++ navy-apps/libs/libam/src/trm.cpp
@@ -1,10 +1,11 @@
 #include <am.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define HEAP_SIZE (8 * 1024 * 1024)
 static unsigned char _heap[HEAP_SIZE];
 Area heap = {_heap, _heap + HEAP_SIZE};
 
-void putch(char ch) { putchar(ch); }
+void putch(char ch) { write(1, &ch, 1); }
 
 void halt(int code) { exit(code); }
