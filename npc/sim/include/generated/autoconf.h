4a59e89c55ac6d61faef0a19d5fb95fcf6e04cc1
tracer-ysyx <tracer@ysyx.org>
2025-08-29 17:23:17 +0800

    > sim RTL ysyx_25030081 余意 Linux archlinux 6.16.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 23 Aug 2025 15:32:49 +0000 x86_64 GNU/Linux  17:23:17 up 2 days,  2:50,  1 user,  load average: 1.41, 1.42, 1.32

--- npc/sim/include/generated/autoconf.h
+++ npc/sim/include/generated/autoconf.h
@@ -5,4 +5,12 @@
  *
  */
 #define CONFIG_MSIZE 0x8000000
+#define CONFIG_ETRACE 1
+#define CONFIG_ITRACE 1
+#define CONFIG_DIFFTEST 1
 #define CONFIG_MBASE 0x80000000
+#define CONFIG_FTRACE 1
+#define CONFIG_MTRACE 1
+#define CONFIG_DTRACE 1
+#define CONFIG_DIFFTEST_REF_PATH "tools/difftest/riscv32-nemu-interpreter-so"
+#define CONFIG_TRACE 1
