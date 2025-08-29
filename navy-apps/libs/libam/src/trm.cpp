fbbf4df153f1dca077ad1846ea1ffef83fbfb633
tracer-ysyx <tracer@ysyx.org>
2025-08-29 18:01:22 +0800

    > compile NEMU ysyx_25030081 余意 Linux bogon 6.16.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Sat, 23 Aug 2025 15:32:49 +0000 x86_64 GNU/Linux  18:01:22 up 2 days,  3:28,  1 user,  load average: 0.94, 0.77, 0.71

--- navy-apps/libs/libam/src/trm.cpp
+++ navy-apps/libs/libam/src/trm.cpp
@@ -6,6 +6,6 @@
 static unsigned char _heap[HEAP_SIZE];
 Area heap = {_heap, _heap + HEAP_SIZE};
 
-// void putch(char ch) {}
+void putch(char ch) {}
 
-// void halt(int code) { exit(code); }
+void halt(int code) { exit(code); }
